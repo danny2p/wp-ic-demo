5ed6ecb6a1a90d38f262bd1569eabcf7ecf889fb
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2021-10-26 22:13:22 -0600

    add some markup

--- web/wp-content/plugins/custom/dp-plugin-demo/dp-plugin-demo.php
+++ web/wp-content/plugins/custom/dp-plugin-demo/dp-plugin-demo.php
@@ -9,3 +9,26 @@ Author URI:  https://pantheon.io
 License:     GPL2 etc
 License URI: https://google.com
 */
+
+/*Use this function to replace a single word*/
+function dp_plugin_demo_wordpress_typo_fix( $text ) {
+	return str_replace( 'wordpress', 'WordPress', $text );
+}
+add_filter( 'the_content', 'dp_plugin_demo_wordpress_typo_fix' );
+
+/*Or use this function to replace multiple words or phrases at once*/
+function dp_plugin_demo_content_replace( $content ) {
+	$search  = array( 'wordpress', 'goat', 'Easter', '70', 'sensational' );
+	$replace = array( 'WordPress', 'coffee', 'Easter holidays', 'seventy', 'extraordinary' );
+	return str_replace( $search, $replace, $content );
+}
+add_filter( 'the_content', 'dp_plugin_demo_content_replace' );
+
+/*Use this function to add a note at the end of your content*/
+function dp_plugin_demo_content_footer_note( $content ) {
+	$content .= '<footer class="content-footer">Footer block that should not be here</footer>';
+	return $content;
+}
+add_filter( 'the_content', 'dp_plugin_demo_content_footer_note' );
+
+?>
