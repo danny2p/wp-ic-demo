68b891ca7875dfee5825b1a1765ae80887de93a8
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2021-10-26 22:07:39 -0600

    Custom plugin placeholder

---
+++ web/wp-content/plugins/custom/dp-plugin-demo/dp-plugin-demo.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: DP Plugin Demo
+Plugin URI:  http://google.com
+Description: This plugin does nothing
+Version:     1.0
+Author:      Danny Pfeiffer
+Author URI:  https://pantheon.io
+License:     GPL2 etc
+License URI: https://google.com
+*/
